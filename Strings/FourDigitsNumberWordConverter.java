7dea0ec0d3df33924282c58b5137d1daff428a1c
kd2-80158 <chhimwalsaurabh@gmail.com>
2024-03-13 09:57:37 +0530

    new string questions added

---
+++ Strings/FourDigitsNumberWordConverter.java
@@ -0,0 +1,43 @@
+// package Strings;
+
+public class FourDigitsNumberWordConverter {
+    
+
+    public static String convertNumberToString(int num)
+    {
+        String[] units = {"","one","two","three","four","five","six","seven","eight","nine"};
+        String[] teens = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eigtheen","nineteen"};
+        String[] tens = {"","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+
+        String word = "";
+
+        if(num<10)
+        {
+            word = units[num];
+        }
+        else if(num<20)
+        {
+            word = teens[num-10];
+        }
+        else if(num<100)
+        {
+            word = tens[num/10]+ " "+units[num%10];
+        }
+        else if(num<1000){
+            word = units[num/100] + " Hundred "+ convertNumberToString(num%100);
+        }
+        else
+        {
+            word = units[num/1000] + " Thousand " + convertNumberToString(num%1000); 
+        } 
+              
+        return word;
+    }
+
+    public static void main(String[] args) {
+        int num =9991;
+
+        String result = convertNumberToString(num);
+        System.out.println(result);
+    }
+}
---
+++ Strings/NumberToWordConverter.java
@@ -0,0 +1,49 @@
+// package Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberToWordConverter {
+
+    public static Map<Character,String> digitMap = new HashMap<>();
+    
+    static{
+        digitMap.put('0',"zero");
+        digitMap.put('1',"one");
+        digitMap.put('2',"two");
+        digitMap.put('3',"three");
+        digitMap.put('4',"four");
+        digitMap.put('5',"five");
+        digitMap.put('6', "six");
+        digitMap.put('7',"seven");
+        digitMap.put('8',"eight");
+        digitMap.put('9',"nine");
+    }
+
+    public static String convertNumberToWord(int number)
+    {
+         String word = String.valueOf(number);
+         StringBuilder result = new StringBuilder();
+
+         for(int i=0;i<word.length();i++)
+         {
+            char digit = word.charAt(i);
+            if(digitMap.containsKey(digit))
+            {
+                result.append(digitMap.get(digit));
+            }
+            else
+                result.append(digit);
+         }
+         return result.toString();
+    }
+
+    public static void main(String[] args) {
+        int number = 785;
+        
+        String result = convertNumberToWord(number);
+
+        System.out.println(result);
+    }
+    
+}
